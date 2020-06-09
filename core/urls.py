26b67bec23a01afc62c4ea1c3ead61b5f1827a0a
safwanvk <safwanvalakundil@gmail.com>
2020-06-08 22:19:08 -0700

    Set remove-from-cart

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ItemDetailView, add_to_cart
+from .views import HomeView, ItemDetailView, add_to_cart, remove_from_cart
 from . import *
 
 app_name = 'core'
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', views.checkout, name='checkout'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
-
-
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
 ]
--- core/views.py
+++ core/views.py
@@ -48,7 +48,25 @@ def add_to_cart(request, slug):
     return redirect("core:product", slug=slug
     )
 
-
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.item.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False)[0]
+            order.item.remove(order_item)
+        else:
+            # add a message saying the user doesnt have an order
+            return redirect("core:product", slug=slug) 
+    else:
+        # add a message saying the user doesnt have an order
+        return redirect("core:product", slug=slug)        
+    return redirect("core:product", slug=slug)
 
 
 
