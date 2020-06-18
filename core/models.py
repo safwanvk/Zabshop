f56b8448a9e79d7e07b270b155ce2ab1a0c50495
safwanvk <safwanvalakundil@gmail.com>
2020-06-18 18:45:46 +0530

    Modified model Address

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
+from django_countries.fields import CountryField
 
 
 
@@ -92,12 +93,13 @@ class Address(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     street_address = models.CharField(max_length=100)
     apartment_address = models.CharField(max_length=100)
+    countries = CountryField(multiple=True)
     zip = models.CharField(max_length=100)
-    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
-    default = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.user.username
 
-    class Meta:
-        verbose_name_plural = 'Addresses'
+
+
+
