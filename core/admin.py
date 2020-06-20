d8a7a512d72b996cdd78b1e86d15e7293fcf961e
safwanvk <safwanvalakundil@gmail.com>
2020-06-20 21:09:16 +0530

    Customize admin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user', 'ordered']
+    list_display = ['user',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'billing_address',
+                    'payment',
+                    'coupon'
+                    ]
+    list_display_links = [
+        'user',
+        'billing_address',
+        'payment',
+        'coupon']
+    list_filter = ['user',
+                   'ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
