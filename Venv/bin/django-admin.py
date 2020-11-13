26c203ca87206a37eff34fe3c5348f597b6e394a
safwanvk <safwanvalakundil@gmail.com>
2020-11-13 16:28:17 +0530

    :deploy: To heroku

--- Venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/safwan/projects/Ecommerse-Website-with-django/Venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- ecomm/settings.py
+++ ecomm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STRIPE_PUBLIC_KEY = ''
 STRIPE_SECRET_KEY = ''
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
